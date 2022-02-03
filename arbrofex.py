911ec6edd3d05d47a95796236762ed4b7021e988
toledogji <toledogji@gmail.com>
2022-02-03 16:59:57 -0300

    Minor updates & unit test for cache update functions.

--- arbrofex.py
+++ arbrofex.py
@@ -11,6 +11,7 @@ pyRofex.initialize(user=ra.USER,
                    account=ra.ACCOUNT,
                    environment=pyRofex.Environment.REMARKET)
 
+#Inicializo el diccionario que me permite guardar las mejores tasas de interes.
 ir_cache = {
     "lowest_ask_interest_rate" : None,
     "lair_ticker": None,
@@ -18,69 +19,52 @@ ir_cache = {
     "hbir_ticker": None,
 }
 
+#Inicializo el diccinario que guarda todas las tasas de interes
 interest_rates = {}
-# Handlers para procesar los mensajes y excepciónes.
 
+# Handlers para procesar los mensajes y excepciónes.
 def market_data_handler(message):
     #Extraigo los datos que vienen en el mensaje
     symbol = message["instrumentId"]["symbol"]
     maturity = pyRofex.get_instrument_details(symbol)["instrument"]["maturityDate"]
+    spot_price = mo.get_spot_price(instruments[symbol])
     bid_price = None
     ask_price = None
-    if(message["marketData"]["BI"] and message["marketData"]["BI"] != None):
-        bid_price = message["marketData"]["BI"][0]["price"]
-    if(message["marketData"]["OF"] and message["marketData"]["BI"] != None):
-        ask_price = message["marketData"]["OF"][0]["price"]
-
-    #Printeo los datos para visualizarlos
-    print("Ticker:", symbol)
-    #print("Maturity:", maturity)
-    print("BID: ", bid_price)
-    print("ASK: ", ask_price)
-    
-    #Obtengo el precio spot de cada uno de los instrumentos
-    spot_price = mo.get_spot_price(instruments[symbol])
-    print("SPOT:", spot_price)
-
+    #Inicializo el diccionario donde se van a guardar los datos 
+    #de las tasas de interes para cada instrumento
     interest_rates[symbol] = {
         "ask_interest_rate": None,
         "bid_interest_rate": None,
     }
+
+    if(message["marketData"]["BI"] and message["marketData"]["BI"] != None):
+        bid_price = message["marketData"]["BI"][0]["price"]
+    if(message["marketData"]["OF"] and message["marketData"]["BI"] != None):
+        ask_price = message["marketData"]["OF"][0]["price"]
+    
+    #Muestro los datos de insturmento
+    print("Ticker:", symbol)
+    print("BID: ", bid_price)
+    print("ASK: ", ask_price)    
+    print("SPOT:", spot_price)
+
     #Calculo las tasa de interes implicitas
-   
     if(ask_price is not None):
         ask_interest_rate = mo.get_interest_rate(spot_price, ask_price, maturity)
         interest_rates[symbol]["ask_interest_rate"] = ask_interest_rate
-
-        if(ir_cache["lowest_ask_interest_rate"] is None 
-            or ir_cache["lowest_ask_interest_rate"] > ask_interest_rate ):
-            ir_cache["lowest_ask_interest_rate"] = ask_interest_rate
-            ir_cache["lair_ticker"] = symbol
-            
-        if( ir_cache["lair_ticker"] == symbol and 
-            ir_cache["lowest_ask_interest_rate"] < ask_interest_rate):
-            irates_result = mo.search_interest_rates(interest_rates)
-            ir_cache["lowest_ask_interest_rate"] = irates_result["lair"]
-            ir_cache["lair_ticker"] = irates_result["lair_ticker"]
-
         print("TASA IMPLICITA TOMADORA:", ask_interest_rate, '%')
+
+        ir_cache_updated = mo.update_lair_cache(ir_cache, ask_interest_rate, symbol, interest_rates)
+        ir_cache.update(ir_cache_updated)
         
     if(bid_price is not None):
         bid_interest_rate = mo.get_interest_rate(spot_price, bid_price, maturity)
         interest_rates[symbol]["bid_interest_rate"] = bid_interest_rate
-
-        if( ir_cache["highest_bid_interest_rate"] is None 
-            or ir_cache["highest_bid_interest_rate"] < bid_interest_rate):
-            ir_cache["highest_bid_interest_rate"] = bid_interest_rate
-            ir_cache["hbir_ticker"] = symbol
-
-        if( ir_cache["hbir_ticker"] == symbol and
-            ir_cache["highest_bid_interest_rate"] > bid_interest_rate):
-            irates_result = mo.search_interest_rates(interest_rates)
-            ir_cache["highest_bid_interest_rate"] = irates_result["hbir"]
-            ir_cache["hbir_ticker"] = irates_result["hbir_ticker"]
-
         print("TASA IMPLICITA COLOCADORA:", bid_interest_rate, "%")
+
+        ir_cache_updated = mo.update_hbir_cache(ir_cache, bid_interest_rate, symbol, interest_rates)
+        ir_cache.update(ir_cache_updated)
+
 
     print("Mejor tasa tomadora", ir_cache["lowest_ask_interest_rate"], ir_cache["lair_ticker"], "Mejor tasa colocadora", ir_cache["highest_bid_interest_rate"], ir_cache["hbir_ticker"] )
     mo.search_arbitrage_oportunity(ir_cache)
@@ -96,14 +80,11 @@ pyRofex.init_websocket_connection(market_data_handler=market_data_handler,
                                   error_handler=error_handler,
                                   exception_handler=exception_handler)
 
-# Lista de instrumentos a suscribir
-tickers = ["GGAL/FEB22", "PAMP/FEB22", "YPFD/FEB22", "DLR/FEB22"]
-
 # Defino a que datos me voy a suscribir
 entries = [pyRofex.MarketDataEntry.BIDS,
            pyRofex.MarketDataEntry.OFFERS,
            pyRofex.MarketDataEntry.LAST]
 
 # Recibo datos del mercado
-pyRofex.market_data_subscription(tickers=tickers,
+pyRofex.market_data_subscription(tickers=instruments,
                                  entries=entries)
--- market_operations.py
+++ market_operations.py
@@ -21,12 +21,14 @@ def get_spot_price(symbol):
     todays_data = ticker.history(period='1d')
     return todays_data['Close'][0]
 
+#Calculo la diferencia entre la fecha de vencimiento del instrumento y la fecha actual
 def date_diff(maturity_date):
     current_date = datetime.now()
     maturity_date = datetime.strptime(maturity_date, '%Y%m%d')
     difference = abs((maturity_date - current_date).days)
     return difference
 
+#Calcula la tasa de interes en base al precio spot, futuro y la diferencia al vencimiento del instrumento.
 def calculate_interest_rate(spot, future, difference):
     """
     Calcula la tasa de interes implicita.
@@ -39,11 +41,13 @@ def calculate_interest_rate(spot, future, difference):
     nominal_annual_rate = round(( interest_rate * (365 / difference) ) * 100, 2)
     return nominal_annual_rate
 
+#Obtiene la tasa de interes para el instrumento
 def get_interest_rate(spot, future, maturity):
     difference = date_diff(maturity)
     interest_rate = calculate_interest_rate(spot, future, difference)
     return interest_rate
 
+#Evalua si hay una oportunidad de arbitraje 
 def search_arbitrage_oportunity(ir_cache):
     if(ir_cache["lowest_ask_interest_rate"] < ir_cache["highest_bid_interest_rate"]):
         print("Oportunidad de arbitraje tomando en", ir_cache["lair_ticker"], "al", ir_cache["lowest_ask_interest_rate"], 
@@ -51,6 +55,7 @@ def search_arbitrage_oportunity(ir_cache):
     else:
         print("No hay oportunidad de arbitraje")
 
+#Busca dentro de diccionario donde estan las tasas de interes por cada instrumento, las tasas de interes más convenientes.
 def search_interest_rates(interest_rates):
     ir_result = {
         "lair": None,
@@ -69,6 +74,50 @@ def search_interest_rates(interest_rates):
             ir_result["hbir"] = bid_interest_rate
             ir_result["hbir_ticker"] = ticker
     return ir_result
+
+#Actualiza el lowest ask interest rate en la cache
+def update_lair_cache(ir_cache, ask_interest_rate, symbol, interest_rates):
+    """
+    Actualiza el lowest ask interest rate en la cache.
+    
+    >>> update_lair_cache({"lowest_ask_interest_rate" : 34.38,"lair_ticker": "GGAL/FEB22","highest_bid_interest_rate" : 32,"hbir_ticker": "PAMP/FEB22"},35,"GGAL/FEB22", {'GGAL/FEB22': {'ask_interest_rate': 35, 'bid_interest_rate': None}, 'YPFD/FEB22': {'ask_interest_rate': 37.03, 'bid_interest_rate': 30.36}, 'DLR/FEB22': {'ask_interest_rate': 55.63, 'bid_interest_rate': None}, 'PAMP/FEB22': {'ask_interest_rate': 36, 'bid_interest_rate': 33}})
+    {'lowest_ask_interest_rate': 35, 'lair_ticker': 'GGAL/FEB22', 'highest_bid_interest_rate': 32, 'hbir_ticker': 'PAMP/FEB22'}
+    
+    """
+    if(ir_cache["lowest_ask_interest_rate"] is None 
+            or ir_cache["lowest_ask_interest_rate"] > ask_interest_rate ):
+            ir_cache["lowest_ask_interest_rate"] = ask_interest_rate
+            ir_cache["lair_ticker"] = symbol
+            
+    if( ir_cache["lair_ticker"] == symbol and 
+        ir_cache["lowest_ask_interest_rate"] < ask_interest_rate):
+        irates_result = search_interest_rates(interest_rates)
+        ir_cache["lowest_ask_interest_rate"] = irates_result["lair"]
+        ir_cache["lair_ticker"] = irates_result["lair_ticker"]
+        
+    return ir_cache
+
+#Actualiza el highest bid interest rate en la cache
+def update_hbir_cache(ir_cache, bid_interest_rate, symbol, interest_rates):
+    """
+    Actualiza el highest bid interest rate en la cache.
+    
+    >>> update_hbir_cache({"lowest_ask_interest_rate" : 34.38,"lair_ticker": "GGAL/FEB22","highest_bid_interest_rate" : 32,"hbir_ticker": "PAMP/FEB22"},31.3,"PAMP/FEB22", {'GGAL/FEB22': {'ask_interest_rate': None, 'bid_interest_rate': None}, 'YPFD/FEB22': {'ask_interest_rate': 37.03, 'bid_interest_rate': 30.36}, 'DLR/FEB22': {'ask_interest_rate': 55.63, 'bid_interest_rate': None}, 'PAMP/FEB22': {'ask_interest_rate': 34.38, 'bid_interest_rate': 31.3}})
+    {'lowest_ask_interest_rate': 34.38, 'lair_ticker': 'GGAL/FEB22', 'highest_bid_interest_rate': 31.3, 'hbir_ticker': 'PAMP/FEB22'}
+    
+    """
+    if( ir_cache["highest_bid_interest_rate"] is None 
+            or ir_cache["highest_bid_interest_rate"] < bid_interest_rate):
+            ir_cache["highest_bid_interest_rate"] = bid_interest_rate
+            ir_cache["hbir_ticker"] = symbol
+
+    if( ir_cache["hbir_ticker"] == symbol and
+        ir_cache["highest_bid_interest_rate"] > bid_interest_rate):
+        irates_result = search_interest_rates(interest_rates)
+        ir_cache["highest_bid_interest_rate"] = irates_result["hbir"]
+        ir_cache["hbir_ticker"] = irates_result["hbir_ticker"]
+        
+    return ir_cache
 
 if __name__ == "__main__":
     import doctest
